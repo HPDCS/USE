2ff63127160a057f8820233140772cd34b9aef12
Alessandro Pellegrini <pellegrini@dis.uniroma1.it>
2015-09-22 12:58:07 +0100

    still removed throttling

--- core/main.c
+++ core/main.c
@@ -26,7 +26,7 @@ void *start_thread(void *args) {
 }
 
 void start_simulation(unsigned short int number_of_threads) {
-    pthread_t p_tid[number_of_threads];//pthread_t p_tid[number_of_threads - 1];//
+    pthread_t p_tid[number_of_threads-1];//pthread_t p_tid[number_of_threads];//
     int ret, i;
 
     //Child thread
@@ -36,16 +36,16 @@ void start_simulation(unsigned short int number_of_threads) {
             abort();
         }
     }
-    
+/*    
    if( (ret = pthread_create(&p_tid[number_of_threads - 1], NULL, tuning, NULL)) != 0) {
        fprintf(stderr, "%s\n", strerror(errno));
        abort();
    }
-
+*/
     //Main thread
     thread_loop(0);
 
-    for(i = 0; i < number_of_threads; i++){//for(i = 0; i < number_of_threads - 1; i++){
+    for(i = 0; i < number_of_threads-1; i++){//for(i = 0; i < number_of_threads - 1; i++){
         pthread_join(p_tid[i], NULL);
     }
 }
