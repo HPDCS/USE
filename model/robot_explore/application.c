d9594678ad188d2a2b4660e00c6fab94f0f4c1b6
Mauro Ianni <mauroianni@gmail.com>
2015-09-29 18:08:01 +0000

    patched robot_explore

--- model/robot_explore/application.c
+++ model/robot_explore/application.c
@@ -12,10 +12,56 @@ unsigned int num_cells;
 #include "agent.h"
 #include "cell.h"
 
+#define getPar(args, i) (((char **)args)[(i)])
+
+
+
+static int seek_param(void **args, char *name) {
+	int i = 0;
+	bool parse_end = false;
+
+	while(true) {
+
+		if(getPar(args, i) == NULL) {
+			parse_end = true;
+			break;
+		}
+
+		if(strcmp(getPar(args, i), name) == 0) {
+			break;
+		}
+
+		i++;
+	}
+
+	if(parse_end) {
+		i = -1;
+	}
+
+	return i;
+}
+
+
+int GetParameterInt(void *args, char *name) {
+
+	int i = seek_param(args, name);
+
+	if(i == -1) {
+		rootsim_error(false, "Parameter %s not set, returning -1...\n", name);
+		return -1;
+	}
+
+	return parseInt(getPar(args, i+1));
+}
+
+bool IsParameterPresent(void *args, char *name) {
+	return (seek_param(args, name) != -1);
+}
 
 void ProcessEvent(int me, simtime_t now, int event_type, void *event_content, int event_size, void *pointer) {
 
 	if(event_type == INIT) {
+		
 		if(!IsParameterPresent(event_content, "num_cells")) {
 			printf("You must specify the number of cells (num_cells)\n");
 		}
