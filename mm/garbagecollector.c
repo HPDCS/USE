f505838b9b0745557aced3b993ad07fb4ef48d6c
mauroianni <mauroianni@github.com>
2017-05-13 00:56:25 +0000

    [STABILE]

--- mm/garbagecollector.c
+++ mm/garbagecollector.c
@@ -16,7 +16,7 @@ struct _free_list
 };
 
 
-#define UNROLLED_FACTOR 100
+#define UNROLLED_FACTOR 10000
 #define USE_POSIX_MEMALIGN 0
 #define USE_GLOBAL_LIST 0
 
@@ -36,7 +36,9 @@ void* alloc_array_nodes(hpdcs_gc_status *status, unsigned int num_item)
 {
 	linked_gc_node *res, *tmp;
 	unsigned long long res_tmp;
-	res = malloc( num_item*(status->block_size +HEADER_SIZE) + CACHE_LINE_SIZE);
+	//res = malloc( num_item*(status->block_size +HEADER_SIZE) + CACHE_LINE_SIZE);
+	res = calloc( 1,num_item*(status->block_size +HEADER_SIZE) + CACHE_LINE_SIZE);
+	
 	
 	if(res == NULL)
 		return NULL;
