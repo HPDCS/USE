3dd490b8bb9b19296724717fc373d659a897a91c
mauroianni <mauroianni@github.com>
2017-05-21 22:27:11 +0000

    aggiornati i test su phold

--- launch_phold_retry.sh
+++ launch_phold_retry.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 LP_list="1024"					#numero di lp
-THREAD_list="1 4 8 16 32"		#numero di thread
+THREAD_list="1 4 8 16 24 32"		#numero di thread
 TEST_list="phold"				#test
-RUN_list="1 2 3 4"				#lista del 
+RUN_list="1 2 3 4 5"				#lista del 
 numero di run
 
 FAN_OUT_list="1 50"		#lista fan out
-LOOKAHEAD_list="0.1" #y 0.001"		#lookahead
-LOOP_COUNT_list="600 400 250 150 100 50"	#loop_count
+LOOKAHEAD_list="0.1 0.001"		#lookahead
+LOOP_COUNT_list="50 100 150 250 400 600"	#loop_count
 
 PUB_list="0.33"
-EPB_list="12"
+EPB_list="12 24"
 
 MAX_RETRY="10"
 
@@ -34,40 +34,23 @@ for lookahead in $LOOKAHEAD_list
 do
 	for test in $TEST_list 
 	do
-		make $test 			LOOKAHEAD=${lookahead} FAN_OUT=${fan_out} LOOP_COUNT=${loop_count} 
-		mv $test ${test}_sl_nohi
-		
-		make $test NBC=1 	LOOKAHEAD=${lookahead} FAN_OUT=${fan_out} LOOP_COUNT=${loop_count}  PERC_USED_BUCKET=${pub} ELEM_PER_BUCKET=${epb}
+		make $test NBC=1 	LOOKAHEAD=${lookahead} FAN_OUT=${fan_out} LOOP_COUNT=${loop_count}  PERC_USED_BUCKET=${pub} ELEM_PER_BUCKET=${epb} REPORT=0 DEBUG=0 SPERIMENTAL=1
 		mv $test ${test}_lf_nohi
 		
-		make $test 			REVERSIBLE=1 LOOKAHEAD=${lookahead} FAN_OUT=${fan_out} LOOP_COUNT=${loop_count}
-		mv $test ${test}_sl_hi
-							
-		make $test NBC=1 	REVERSIBLE=1 LOOKAHEAD=${lookahead} FAN_OUT=${fan_out} LOOP_COUNT=${loop_count}  PERC_USED_BUCKET=${pub} ELEM_PER_BUCKET=${epb}
+		make $test NBC=1 	REVERSIBLE=1 LOOKAHEAD=${lookahead} FAN_OUT=${fan_out} LOOP_COUNT=${loop_count}  PERC_USED_BUCKET=${pub} ELEM_PER_BUCKET=${epb} REPORT=0 DEBUG=0 SPERIMENTAL=1
 		mv $test ${test}_lf_hi
+		
 		for run in $RUN_list
 		do
 			for lp in $LP_list
 				do
 					for threads in $THREAD_list
 					do
-						EX1="./${test}_sl_nohi $threads $lp"
 						EX2="./${test}_lf_nohi $threads $lp"
-						EX3="./${test}_sl_hi $threads $lp"
 						EX4="./${test}_lf_hi $threads $lp"
-						FILE1="${FOLDER}/${test}-sl-dymelor-nohijacker-$threads-$lp-look-$lookahead-fan-$fan_out-loop-$loop_count-$run"; touch $FILE1
 						FILE2="${FOLDER}/${test}-lf-dymelor-nohijacker-$threads-$lp-look-$lookahead-fan-$fan_out-loop-$loop_count-$run"; touch $FILE2
-						FILE3="${FOLDER}/${test}-sl-dymelor-hijacker-$threads-$lp-look-$lookahead-fan-$fan_out-loop-$loop_count-$run"; touch $FILE3
 						FILE4="${FOLDER}/${test}-lf-dymelor-hijacker-$threads-$lp-look-$lookahead-fan-$fan_out-loop-$loop_count-$run"; touch $FILE4
 						
-						N=0
-						while [[ $(grep -c "Simulation ended" $FILE1) -eq 0 ]]
-						do
-							echo $FILE1
-							$EX1 > $FILE1
-							if test $N -ge $MAX_RETRY ; then echo break; break; fi
-							N=$(( N+1 ))
-						done
 						
 						N=0 
 						while [[ $(grep -c "Simulation ended" $FILE2) -eq 0 ]]
@@ -77,15 +60,6 @@ do
 							if test $N -ge $MAX_RETRY ; then echo break; break; fi
 							N=$(( N+1 ))
 						done
-						
-						N=0 
-						while [[ $(grep -c "Simulation ended" $FILE3) -eq 0 ]]
-						do
-							echo $FILE3
-							$EX3 > $FILE3
-							if test $N -ge $MAX_RETRY ; then echo break; break; fi
-							N=$(( N+1 ))
-						done 
 						
 						N=0 
 						while [[ $(grep -c "Simulation ended" $FILE4) -eq 0 ]]
@@ -100,9 +74,7 @@ do
 					done
 				done
 		done
-		rm ${test}_sl_nohi
 		rm ${test}_lf_nohi
-		rm ${test}_sl_hi
 		rm ${test}_lf_hi
 	done
 done
