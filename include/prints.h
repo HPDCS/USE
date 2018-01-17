bf25a71d44a1714285707d7ad9f91a0ec4078d10
Davide Cingolani <cingolani@diag.uniroma1.it>
2018-01-17 11:22:04 +0100

    Added print_event

--- include/prints.h
+++ include/prints.h
@@ -1,13 +1,21 @@
 #ifndef _PRINTS_H_
 #define _PRINTS_H_
 
-#if VERBOSE != 0
+#if DEBUG == 1
+
 #define printlp(format, ...) printf("T%lu LP%lu :: " format, tid, current_lp, ##__VA_ARGS__);
 #define printth(format, ...) printf("T%lu :: " format, tid, ##__VA_ARGS__);
-#define print_event(event)	printf("   [LP:%u->%u]: TS:%f TB:%u EP:%u IS_VAL:%u\n",event->sender_id, event->receiver_id, event->timestamp, event->tie_breaker, event->epoch, is_valid(event));
+
+#define print_event(event)	printf("   [LP:%u->%u]: TS:%f TB:%u EP:%u STATE:%#02x(%s) IS_VAL:%u \t\tEvt.ptr:%p Node.ptr:%p\n", event->sender_id, event->receiver_id, event->timestamp, event->tie_breaker, event->epoch, event->state, evt_state_str(event->state), is_valid(event), event, event->node)
+
 #else
+
 #define printlp(...)
 #define printth(...)
-#endif
+#define print_event(event)
 
-#endif
+#endif // DEBUG == 1
+
+#define evt_state_str(state) state == 1 ? "ESTRATTO" : state == 2 ? "CANCELLATO" : state == 3 ? "ANTI_EVENT" : "UNKNOWN"
+
+#endif // _PRINTS_H_
