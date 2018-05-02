ffd949eb54c2e071bdf131a6d394a4c39a0154b1
mauroianni <mauroianni@gmail.com>
2018-05-02 12:21:55 +0200

    Sistemata la gestione del file topology.txt

--- launch_traffic_retry.sh
+++ launch_traffic_retry.sh
@@ -2,10 +2,10 @@
 
 #MAX_SKIPPED_LP_list="1000000"
 LP_list="1024"					#numero di lp
-THREAD_list="32 24 16 8 4 2" #"4 8 16 24 32"	#numero di thread
-RUN_list="1"					#lista del numero di run
+THREAD_list="32 24 16 8 4 2 1" #"4 8 16 24 32"	#numero di thread
+RUN_list="1 2 3"					#lista del numero di run
 
-LOOKAHEAD_list="0 0.01" #"0 0.1 0.01"	#lookahead
+#LOOKAHEAD_list="0 0.01" #"0 0.1 0.01"	#lookahead
 
 OP_list="0 1 2" #2
 
@@ -20,7 +20,7 @@ MAX_RETRY="10"
 
 FOLDER="results/results_traffic" #/results_phold_$(date +%Y%m%d)-$(date +%H%M)"
 
-INPUT_list="30_01 60_01 30_05 60_05"
+INPUT_list="60_01 30_01 10_01 10_001"
 ENTERP_list="30 60"
 LEAVEP_list="01 05"
 
@@ -43,26 +43,26 @@ mkdir -p ${FOLDER}
 for run in $RUN_list
 do
 
-#for fn in $INPUT_list 
-#do
+for fn in $INPUT_list 
+do
 	for op in $OP_list
 	do
-		for enp in $ENTERP_list
-		do
-			for lvp in $LEAVEP_list
-			do
-				#echo cp topology_${fn}.txt topology.txt
-				#cp model/traffic/topology_${fn}.txt model/traffic/topology.txt
+#		for enp in $ENTERP_list
+#		do
+#			for lvp in $LEAVEP_list
+#			do
+				echo cp topology_${fn}.txt topology.txt
+				cp model/traffic/topology_${fn}.txt topology.txt
 						
-				echo make traffic REPORT=1 DEBUG=0 PRINT_SCREEN=0 ENTER_PROB=${enp} LEAVE_PROB=${lvp} OPTIMISTIC_LEVEL=${op}
-				make traffic REPORT=1 DEBUG=0 PRINT_SCREEN=0 ENTER_PROB=${enp} LEAVE_PROB=${lvp} OPTIMISTIC_LEVEL=${op}
+				echo make traffic REPORT=1 DEBUG=0 PRINT_SCREEN=0 OPTIMISTIC_LEVEL=${op}
+				make traffic REPORT=1 DEBUG=0 PRINT_SCREEN=0 OPTIMISTIC_LEVEL=${op}
 				
 				for lp in $LP_list
 				do
 					for threads in $THREAD_list
 					do
 						EX="./traffic $threads $lp"
-						FILE="${FOLDER}/traffic_${threads}-${lp}_ol${op}_${enp}_${lvp}_${run}.dat"
+						FILE="${FOLDER}/traffic_${threads}-${lp}_ol${op}_${fn}_${run}.dat"
 						touch $FILE
 						
 						N=0 
@@ -78,9 +78,9 @@ do
 					done
 				done
 			done
-		done
-	done
-#done
+#		done
+#	done
+done
 
 done
 #done
