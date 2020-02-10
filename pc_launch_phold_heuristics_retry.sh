84973e9785f38d3dfeea9c309fd218fcaa3ab999
Romolo <romolo.marotta@gmail.com>
2020-02-11 00:14:34 +0100

    ADded new scripts for evaluating heuristics

---
+++ pc_launch_phold_heuristics_retry.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+source ./pc_phold_heuristic.config
+
+
+mkdir -p ${FOLDER}
+
+for max_lp in $MAX_SKIPPED_LP_list
+do
+for ck in $CKP_PER_list
+do
+for pub in $PUB_list
+do
+for epb in $EPB_list
+do
+for loop_count in $LOOP_COUNT_list
+do
+for fan_out in $FAN_OUT_list
+do
+for lookahead in $LOOKAHEAD_list
+do
+	for filtering in $POWERCAP_SAMPLE_FILTERING_list
+	do
+		for test in $TEST_list 
+		do
+			echo make $test POWERCAP=1 NBC=1 POWERCAP_SAMPLE_FILTERING=$filtering MAX_SKIPPED_LP=${max_lp} REVERSIBLE=0 LOOKAHEAD=${lookahead} FAN_OUT=${fan_out} LOOP_COUNT=${loop_count}  PERC_USED_BUCKET=${pub} ELEM_PER_BUCKET=${epb} REPORT=1 DEBUG=0 SPERIMENTAL=1 CKP_PERIOD=${ck} PRINT_SCREEN=0
+			make $test POWERCAP=1 NBC=1 POWERCAP_SAMPLE_FILTERING=$filtering MAX_SKIPPED_LP=${max_lp} REVERSIBLE=0 LOOKAHEAD=${lookahead} FAN_OUT=${fan_out} LOOP_COUNT=${loop_count}  PERC_USED_BUCKET=${pub} ELEM_PER_BUCKET=${epb} REPORT=1 DEBUG=0 SPERIMENTAL=1 CKP_PERIOD=${ck} PRINT_SCREEN=0
+			mv $test ${test}_lf_hi
+			
+			for pl in $POWER_LIMIT_list
+			do
+				for hmode in $HEURISTIC_MODE
+				do
+					for run in $RUN_list
+					do
+						for lp in $LP_list
+						do
+							for pstate in $PSTATE_list
+							do
+								for threads in $CORES
+								do
+								
+									./create_config.sh $threads $pstate $hmode $pl
+									EX="sudo ./${test}_lf_hi $CORES $lp $TEST_DURATION"
+									FILE="${FOLDER}/${test}-lf-dymelor-hijacker-psf$filtering-w$pl-h$hmode-$threads-p$pstate-$lp-maxlp-$max_lp-look-$lookahead-ck_per-$ck-fan-$fan_out-loop-$loop_count-$run"; touch $FILE
+															
+									N=0 
+									while [[ $(grep -c "Simulation ended" $FILE) -eq 0 ]]
+									do
+										echo $BEGIN
+										echo "CURRENT TEST STARTED AT $(date +%d)/$(date +%m)/$(date +%Y) - $(date +%H):$(date +%M)"
+										echo $FILE
+										#1> $FILE 2>&1 time $EX
+										(time $EX) &> $FILE
+										if test $N -ge $MAX_RETRY ; then echo break; break; fi
+										N=$(( N+1 ))
+									done  
+									
+									  
+								done  
+							done
+						done
+					done
+				done
+			done
+			rm ${test}_lf_hi
+		done
+	done
+done
+done
+done
+done
+done
+done
+done
