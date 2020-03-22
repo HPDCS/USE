42a0dc6e0e835b0f0d85cafcbcc648eee8c38234
cristian <miliacristian4@gmail.com>
2020-03-22 17:57:34 +0100

    added script main_launch2.sh

--- results_and_scripts/main_launch1.sh
+++ results_and_scripts/main_launch1.sh
@@ -4,18 +4,6 @@ machine="totti"
 machine_thread_list=${machine}_thread_list
 test_launcher=0
 
-cd pcs_new
-./launcher_pcs_new_retry.sh ../machine_conf/${machine}.conf pcs_new_after_paper1.conf 0 ${test_launcher}
-./launcher_pcs_new_retry.sh ../machine_conf/${machine}.conf pcs_new_after_paper2.conf 0 ${test_launcher}
-./launcher_pcs_new_retry.sh ../machine_conf/${machine}.conf pcs_new_after_paper3.conf 0 ${test_launcher}
-
-cd ..
-
-cd phold
-./launcher_phold_retry.sh ../machine_conf/${machine_thread_list}.conf phold_after_paper1.conf 0 ${test_launcher}
-
-cd ..
-
 cd phold_multicast
 ./launcher_phold_multicast_retry.sh ../machine_conf/${machine}.conf phold_multicast_after_paper1.conf 0 ${test_launcher}
 
@@ -26,4 +14,5 @@ cd phold_nears
 ./launcher_phold_nears_retry.sh ../machine_conf/${machine}.conf phold_nears_after_paper1.conf 0 ${test_launcher}
 
 cd ..
+
 
---
+++ results_and_scripts/main_launch2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+machine="totti"
+machine_thread_list=${machine}_thread_list
+test_launcher=0
+
+cd pcs_new
+./launcher_pcs_new_retry.sh ../machine_conf/${machine}.conf pcs_new_after_paper1.conf 0 ${test_launcher}
+./launcher_pcs_new_retry.sh ../machine_conf/${machine}.conf pcs_new_after_paper2.conf 0 ${test_launcher}
+./launcher_pcs_new_retry.sh ../machine_conf/${machine}.conf pcs_new_after_paper3.conf 0 ${test_launcher}
+
+cd ..
+
+cd phold
+./launcher_phold_retry.sh ../machine_conf/${machine_thread_list}.conf phold_after_paper1.conf 0 ${test_launcher}
+
+cd ..
+
