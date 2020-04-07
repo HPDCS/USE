6212f3334aa223613728f8676550153b7d190ca8
cristian <miliacristian4@gmail.com>
2020-04-07 17:48:53 +0200

    modified phold_multicast

--- model/phold_multicast/application.c
+++ model/phold_multicast/application.c
@@ -38,8 +38,8 @@ void ProcessEvent(int me, simtime_t now, int event_type, event_content_type *eve
 			state_ptr->events = 0;
 
 			if(me == 0) {
-				printf("Running %s a traditional loop-based PHOLD benchmark with counter set to %d, %d total events per LP, lookahead %f,THR_PROB_NORMAL=%lf\n",MODEL_NAME, LOOP_COUNT, COMPLETE_EVENTS, LOOKAHEAD, THR_PROB_NORMAL);
-				printf("lookahead %f,THR_PROB_NORMAL=%lf\n",LOOKAHEAD, THR_PROB_NORMAL);
+				printf("Running %s a traditional loop-based PHOLD benchmark with counter set to %d, %d total events per LP, lookahead %f,thr_prob_normal=%lf\n",MODEL_NAME, LOOP_COUNT, COMPLETE_EVENTS, LOOKAHEAD, THR_PROB_NORMAL);
+				printf("lookahead %f,thr_prob_normal=%lf\n",LOOKAHEAD, THR_PROB_NORMAL);
 			}
 			//TODO print all parameters
 
