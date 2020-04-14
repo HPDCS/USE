3c8fdfb200fb22b01822b1d165999de4118ce67a
cristian <miliacristian4@gmail.com>
2020-04-14 13:51:53 +0200

    modified script pcs_new

---
+++ results_and_scripts/main_launch_pcs_new_totti.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+machine="totti"
+machine_thread_list=${machine}_thread_list
+test_launcher=0
+
+cd pcs_new
+echo "./launcher_pcs_new_retry.sh ../machine_conf/${machine}.conf pcs_new_after_paper1_${machine}.conf 0 ${test_launcher}"
+./launcher_pcs_new_retry.sh ../machine_conf/${machine}.conf pcs_new_after_paper1_${machine}.conf 0 ${test_launcher}
+
+cd ..
+
