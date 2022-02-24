90e56f7d3f297cfa6c80c2fd572012e3e199de59
RomoloMarotta <romolo.marotta@gmail.com>
2022-02-24 15:59:34 +0100

    refactor(build_scripts): improved gen_clock_header script

--- build_scripts/gen_clock_header.sh
+++ build_scripts/gen_clock_header.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 gcc compute_clock.c -o compute_clock
+echo -ne "wainting compute clock..."
 clocks=$(./compute_clock 5 | grep us | cut -d':' -f2)
-mkdir -p ../include-gen
+echo "DONE"
+
 out="clock_constant.h"
 
 echo "#ifndef __CLOCKS_PER_US_H__"     > $out
@@ -11,5 +13,5 @@ echo ""                               >> $out
 echo "#define CLOCKS_PER_US ((unsigned long long)$clocks)"  >> $out
 echo "#endif"                         >> $out
 
-mv $out ../include-gen
+
 rm compute_clock
