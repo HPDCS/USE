fc2926b09307cd62cae98104ceb9a6fe074a5dee
RomoloMarotta <romolomarotta@gmail.com>
2022-03-07 02:28:10 +0000

    fix dyn pcs script

--- launch_pcs_dyn.sh
+++ launch_pcs_dyn.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 MAX_SKIPPED_LP_list="1000000"
-LP_list="1024"                      #number of lps
-THREAD_list="40"        #number of  threads
+LP_list="256 1024 4096"                      #number of lps
+THREAD_list="1 6 12 24 36 48"        #number of  threads
 TEST_list="pcs"                 #test
-RUN_list="1 2 3"                
+RUN_list="1 2 3 4 5"                
 
 ENFORCE_LOCALITY_list="0 1" #DISTRIBUTED_FETCH
 
 TA_CHANGE_list="300"
 TA_DURATION_list="120"
-TA_list="0.16 0.08 0.04"
+TA_list="0.48 0.24 0.12"
 
 LOOKAHEAD_list="0" # 0.01" #"0 0.1 0.01"        #lookahead
 #WINDOW_list="0.1 0.2 0.4 0.8 1.6"
 #CURRENT_BINDING_SIZE="1 2 4 8"
 #EVICTED_BINDING_SIZE="1 2 4 8"
-NCHANNELS_list="400"
+NCHANNELS_list="1000"
 
 
 
@@ -28,7 +28,7 @@ TEST_DURATION="30"
 BEGIN="BEGIN TEST:.............$(date +%d)/$(date +%m)/$(date +%Y) - $(date +%H):$(date +%M)"
 CURRT="CURRENT TEST STARTED AT $(date +%d)/$(date +%m)/$(date +%Y) - $(date +%H):$(date +%M)"
 
-FOLDER="results/pcs_static_win" #/results_phold_$(date +%Y%m%d)-$(date +%H%M)"
+FOLDER="results/pcs_dyn_win_v2" #/results_phold_$(date +%Y%m%d)-$(date +%H%M)"
 
 mkdir -p ${FOLDER}
 
@@ -68,7 +68,7 @@ do
                             do
                                 EX1="./${test} $th $lp ${TEST_DURATION}"
                                 
-                                FILE1="${FOLDER}/${test}_el_${df}-w_${w}-nch_${nch}-ta_${tav}-tad_${tad}-tac_${tac}_th_${th}-lp_${lp}-run_${run}.dat"
+                                FILE1="${FOLDER}/${test}_el_${df}-w_${w}-nch_${nch}-ta_${tav}-tad_${tad}-tac_${tac}-th_${th}-lp_${lp}-run_${run}.dat"
                                 
                                 touch ${FILE1}
                                 #echo $FILE1
