09ca5b3e5ba76904810e61aac59c9b9601c1dbe8
Romolo Marotta <romolo.marotta@gmail.com>
2022-04-14 15:50:43 +0200

    removed warnings

--- model/pcs/application.c
+++ model/pcs/application.c
@@ -15,7 +15,7 @@ double ran;
 
 void ProcessEvent(unsigned int me, simtime_t now, int event_type, event_content_type *event_content, unsigned int size, void *ptr) {
 	unsigned int w;
-
+	(void)size;
 	//printf("%d executing %d at %f\n", me, event_type, now);
 
 	event_content_type new_event_content;
@@ -282,6 +282,7 @@ void ProcessEvent(unsigned int me, simtime_t now, int event_type, event_content_
 
 
 bool OnGVT(unsigned int me, lp_state_type *snapshot) {
+	(void)me;
 	if (snapshot->complete_calls < complete_calls)
 		return false;
 	return true;
