168faf1125dfcca1341fbdba7f41305b6e31c138
Romolo Marotta <romolo.marotta@gmail.com>
2022-11-18 22:22:08 +0100

    [reproducibility]: added last scripts
    Signed-off-by: Romolo Marotta <romolo.marotta@gmail.com>

---
+++ figure9b_run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source autoconf.sh
+./base_scripts/figure9__plot.sh 0.24 $MAX_THREADS
---
+++ figure9c_run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source autoconf.sh
+./base_scripts/figure9__plot.sh 0.12 $MAX_THREADS
