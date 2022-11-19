e179e4c4b7adf1b3af4c49b61cf173f1afdc4272
federicamontes <federica.montesan42@gmail.com>
2022-11-19 15:29:17 +0100

    added check on num_numa_nodes before initializing facilities for numa pinning in numa_init (core/core.c)

--- core/core.c
+++ core/core.c
@@ -344,20 +344,26 @@ void numa_init(){
 		alloc_numa_state(numa_state[i]);
 		numa_state[i]->numa_id = i;
 	}
+
+
 	
 	/// bitmask for each numa node
 	struct bitmask *numa_mask[num_numa_nodes];
-        for (int i=0; i < num_numa_nodes; i++) {
-                numa_mask[i] = numa_allocate_cpumask();
-                numa_node_to_cpus(i, numa_mask[i]);
-        }
+
+	if (num_numa_nodes > 1) {
+
+	    for (int i=0; i < num_numa_nodes; i++) {
+	            numa_mask[i] = numa_allocate_cpumask();
+	            numa_node_to_cpus(i, numa_mask[i]);
+	    }
+	}
 
 	/// set a global array in which are set the cpus on the same numa node
 	/// indexed from 0 to cpu_per_node and from cpu_per_node to N_CPU-1
 	int insert_idx = 0;
 	for (int j=0; j < num_numa_nodes; j++) {
 		for (int k=0; k < N_CPU; k++) {
-			if (numa_bitmask_isbitset(numa_mask[j], k)) {
+			if (num_numa_nodes ==1 || numa_bitmask_isbitset(numa_mask[j], k)) {
 				cores_on_numa[insert_idx] = k; 
 				insert_idx++;
 			}
