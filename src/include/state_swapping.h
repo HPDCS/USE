e887ceb86f21b55323b24e103fe882df6cb6b820
federicamontes <federica.montesan42@gmail.com>
2023-02-15 16:33:46 +0100

    added two variables for worker thread count

--- src/include/state_swapping.h
+++ src/include/state_swapping.h
@@ -6,21 +6,24 @@
 typedef struct state_swapping_struct {
 
 	bitmap *lp_bitmap; /// bitmap representing all the lps
-	unsigned int counter_lp; /// counter for the lps examined
+	int counter_lp; /// counter of the lps examined
 	simtime_t reference_gvt; /// reference gvt for the output collection
 	unsigned int state_swap_flag; /// flag for determining sync csr
-	int worker_threads; /// number of threads executing the csr function
+	int worker_threads_in; /// number of threads executing the csr function
+	int worker_threads_out; /// number of threads executing the csr function
 
 } state_swapping_struct;
 
 extern state_swapping_struct *state_swap_ptr;
 
 extern __thread unsigned int potential_locked_object; /// it contains the index of the lp locked in normal context
-extern __thread unsigned int csr_mode; /// it represents the modality of execution of the csr function
-									   /// it's 0 if it is sync, 1 if async
+
 extern __thread simtime_t commit_horizon_to_save; 
 
-extern void signal_state_swapping();
+#if CSR_CONTEXT == 0
+  extern void signal_state_swapping();
+#endif
+
 extern state_swapping_struct *alloc_state_swapping_struct();
 
 
