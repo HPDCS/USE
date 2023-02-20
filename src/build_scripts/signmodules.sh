63373bb62a057b5f930da8fcc875145c0fb26074
Romolo Marotta <romolo.marotta@gmail.com>
2023-02-20 12:50:06 +0100

    added/updated scripts
    * updated script for signing lk modules
    * added script to get all configuration macros
    
    Signed-off-by: Romolo Marotta <romolo.marotta@gmail.com>

--- src/build_scripts/signmodules.sh
+++ src/build_scripts/signmodules.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 /home/romolo/certs/my_signing_key.priv /home/romolo/certs/my_signing_key_pub.der Debug/src/idt_patcher/idt_patcher.ko
-/usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 /home/romolo/certs/my_signing_key.priv /home/romolo/certs/my_signing_key_pub.der Debug/src/syscall_table_patcher/syscall_table_patcher.ko
-/usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 /home/romolo/certs/my_signing_key.priv /home/romolo/certs/my_signing_key_pub.der Debug/src/kallsyms_lookup_name_exporter/kln_exporter.ko
-/usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 /home/romolo/certs/my_signing_key.priv /home/romolo/certs/my_signing_key_pub.der Debug/src/trap-based-user-context/trap-based-usercontext.ko
+/usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 /home/romolo/certs/my_signing_key.priv /home/romolo/certs/my_signing_key_pub.der /home/romolo/git/USE/Debug/src/idt_patcher/idt_patcher.ko
+/usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 /home/romolo/certs/my_signing_key.priv /home/romolo/certs/my_signing_key_pub.der /home/romolo/git/USE/Debug/src/syscall_table_patcher/syscall_table_patcher.ko
+/usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 /home/romolo/certs/my_signing_key.priv /home/romolo/certs/my_signing_key_pub.der /home/romolo/git/USE/Debug/src/kallsyms_lookup_name_exporter/kln_exporter.ko
+/usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 /home/romolo/certs/my_signing_key.priv /home/romolo/certs/my_signing_key_pub.der /home/romolo/git/USE/Debug/src/trap-based-user-context/trap-based-usercontext.ko
