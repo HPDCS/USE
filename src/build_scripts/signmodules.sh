0d90538e5f3267b6b9f50306bc6dd0fc9016b68e
Romolo Marotta <romolo.marotta@gmail.com>
2023-02-27 03:46:22 +0100

    [CSR]: improved signing script for kernel modules
    Signed-off-by: Romolo Marotta <romolo.marotta@gmail.com>

--- src/build_scripts/signmodules.sh
+++ src/build_scripts/signmodules.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ver=Release
+ver=$1
 
 /usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 /home/romolo/certs/my_signing_key.priv /home/romolo/certs/my_signing_key_pub.der /home/romolo/git/USE/$ver/src/idt_patcher/idt_patcher.ko
 /usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 /home/romolo/certs/my_signing_key.priv /home/romolo/certs/my_signing_key_pub.der /home/romolo/git/USE/$ver/src/syscall_table_patcher/syscall_table_patcher.ko
