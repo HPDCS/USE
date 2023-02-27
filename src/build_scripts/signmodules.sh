287d57aff506474bed0af79ad067cbf821b78e25
Romolo Marotta <romolo.marotta@gmail.com>
2023-02-27 03:05:04 +0100

    improved script for signing modules
    Signed-off-by: Romolo Marotta <romolo.marotta@gmail.com>

--- src/build_scripts/signmodules.sh
+++ src/build_scripts/signmodules.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-/usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 /home/romolo/certs/my_signing_key.priv /home/romolo/certs/my_signing_key_pub.der /home/romolo/git/USE/Debug/src/idt_patcher/idt_patcher.ko
-/usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 /home/romolo/certs/my_signing_key.priv /home/romolo/certs/my_signing_key_pub.der /home/romolo/git/USE/Debug/src/syscall_table_patcher/syscall_table_patcher.ko
-/usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 /home/romolo/certs/my_signing_key.priv /home/romolo/certs/my_signing_key_pub.der /home/romolo/git/USE/Debug/src/kallsyms_lookup_name_exporter/kln_exporter.ko
-/usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 /home/romolo/certs/my_signing_key.priv /home/romolo/certs/my_signing_key_pub.der /home/romolo/git/USE/Debug/src/trap-based-user-context/trap-based-usercontext.ko
+ver=Release
+
+/usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 /home/romolo/certs/my_signing_key.priv /home/romolo/certs/my_signing_key_pub.der /home/romolo/git/USE/$ver/src/idt_patcher/idt_patcher.ko
+/usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 /home/romolo/certs/my_signing_key.priv /home/romolo/certs/my_signing_key_pub.der /home/romolo/git/USE/$ver/src/syscall_table_patcher/syscall_table_patcher.ko
+/usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 /home/romolo/certs/my_signing_key.priv /home/romolo/certs/my_signing_key_pub.der /home/romolo/git/USE/$ver/src/kallsyms_lookup_name_exporter/kln_exporter.ko
+/usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 /home/romolo/certs/my_signing_key.priv /home/romolo/certs/my_signing_key_pub.der /home/romolo/git/USE/$ver/src/trap-based-user-context/trap-based-usercontext.ko
