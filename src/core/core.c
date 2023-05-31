bb40cf60e27b69ae5aab27968c3393489de46425
federicamontes <federica.montesan42@gmail.com>
2023-05-31 16:44:08 +0200

    refactor(core.c): added init functions for incremental checkpointing
    * init_incremental_checkpointing_support, init_incremental_checkpoint_support_per_lp and iss_first_run_model executed if we run with INCREMENTAL_STATE_SAVING enabled
    
    * iss_first_run_model is executed before taking LogState after the first ProcessEvent for each lp

--- src/core/core.c
+++ src/core/core.c
@@ -492,6 +492,10 @@ void init_simulation(unsigned int thread_id){
 		numerical_init();
 		nodes_init();
 
+		if (pdes_config.checkpointing == INCREMENTAL_STATE_SAVING) {
+			init_incremental_checkpoint_support(pdes_config.nprocesses);
+		}
+
 		if(pdes_config.enforce_locality){
 			pthread_barrier_init(&local_schedule_init_barrier, NULL, pdes_config.ncores);
 			init_metrics_for_window();
@@ -516,6 +520,10 @@ void init_simulation(unsigned int thread_id){
 		current_lp = __sync_fetch_and_add(&lp_inizialized, 1);
 		if(current_lp >=  pdes_config.nprocesses) continue;
 		if(!pdes_config.serial) allocator_init_for_lp(current_lp);
+		if (pdes_config.checkpointing == INCREMENTAL_STATE_SAVING){
+			init_incremental_checkpoint_support_per_lp(current_lp);
+		}
+
 		current_msg = list_allocate_node_buffer_from_list(current_lp, sizeof(msg_t), (struct rootsim_list*) freed_local_evts);
  		current_msg->sender_id 		= -1;//
  		current_msg->receiver_id 	= current_lp;//
@@ -531,6 +539,9 @@ void init_simulation(unsigned int thread_id){
 			LPS[current_lp]->bound = current_msg;
 			LPS[current_lp]->num_executed_frames++;
 			LPS[current_lp]->state_log_forced = true;
+			if (pdes_config.checkpointing == INCREMENTAL_STATE_SAVING) {
+				iss_first_run_model(current_lp);
+			}
 			LogState(current_lp);
 			((state_t*)((rootsim_list*)LPS[current_lp]->queue_states)->head->data)->lvt = -1;// Required to exclude the INIT event from timeline
 			LPS[current_lp]->state_log_forced = false;
