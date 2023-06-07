0850cf671e3fb689d64cf7372b256bc77fa603b7
federicamontes <federica.montesan42@gmail.com>
2023-06-07 13:22:00 +0200

    refactored code across multiple files to change iss_enabled_protection in iss_mode
    files: incremental_state_saving.c
           recoverable.c
           test/incremental_state_saving.c
           checkpoints.c

--- src/mm/recoverable.c
+++ src/mm/recoverable.c
@@ -109,7 +109,7 @@ size_t get_log_size(malloc_state *logged_state){
 		log_size = sizeof(malloc_state) + sizeof(seed_type) + logged_state->busy_areas * sizeof(malloc_area) + logged_state->bitmap_size + logged_state->total_log_size;
 	} else {
 
-		if (pdes_config.iss_enabled_mprotection == DDYMELOR) {
+		if (pdes_config.iss_mode == DDYMELOR) {
 			log_size = sizeof(malloc_state) + sizeof(seed_type) + logged_state->dirty_areas * sizeof(malloc_area) + logged_state->bitmap_size*2 + logged_state->total_inc_size;
 		} else {
 			log_size = sizeof(malloc_state) + sizeof(seed_type) + logged_state->busy_areas * sizeof(malloc_area) + logged_state->bitmap_size + sizeof(void *);
