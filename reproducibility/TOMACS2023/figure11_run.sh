11d207dec3e80c58834096645f2ee062b9d4035b
Romolo Marotta <romolo.marotta@gmail.com>
2023-11-17 00:54:27 +0000

    added script fr fig 11
    Signed-off-by: Romolo Marotta <romolo.marotta@gmail.com>

--- reproducibility/TOMACS2023/figure11_run.sh
+++ reproducibility/TOMACS2023/figure11_run.sh
@@ -14,24 +14,30 @@ cmd="use-release/test/test_pcs --nprocesses=$lp --ckpt-autonomic-period --enable
 
 
 res="results/pcs-dynhot"
+mkdir -p $res
 
 cur="$cmd --ncores=1"
 f="$res/seq_hs-1-$lp-$MIN-$r"
+#echo $cur > $f.sh
+#./$cur > $f.dat
+
+cur="$cmd --ncores=${MAX_THREADS}"
+f="$res/pcs_hs-${MAX_THREADS}-$lp-$MIN-$r"
 echo $cur > $f.sh
 ./$cur > $f.dat
 
-f="$res/pcs_hs-${MAX_THREADS}-$lp-$MIN-$r"
+
 cmd="$cmd --enforce-locality --el-dyn-window --el-locked-size=2 --el-evicted-size=2"
 cur="$cmd --ncores=${MAX_THREADS}"
-f="pcs_hs_lo-${MAX_THREADS}-$lp-$MIN-$r"
-echo $cur > $f.sh
-./$cur > $f.dat
+f="$res/pcs_hs_lo-${MAX_THREADS}-$lp-$MIN-$r"
+#echo $cur > $f.sh
+#./$cur > $f.dat
 
 cmd="$cmd --numa-rebalance --distributed-fetch"
 cur="$cmd --ncores=${MAX_THREADS}"
-f="pcs_hs_lo_re_df-${MAX_THREADS}-$lp-$MIN-$r"
-echo $cur > $f.sh
-./$cur > $f.dat
+f="$res/pcs_hs_lo_re_df-${MAX_THREADS}-$lp-$MIN-$r"
+#echo $cur > $f.sh
+#./$cur > $f.dat
 
 done
 done
