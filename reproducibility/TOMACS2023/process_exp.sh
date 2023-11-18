456fb4bdc98d889d15add2d2e83854acd8f1b410
Romolo Marotta <romolo.marotta@gmail.com>
2023-11-18 19:23:48 +0100

    repro(tomacs23): improved process and run all
    Signed-off-by: Romolo Marotta <romolo.marotta@gmail.com>

--- reproducibility/TOMACS2023/process_exp.sh
+++ reproducibility/TOMACS2023/process_exp.sh
@@ -1,10 +1,56 @@
+#!/bin/bash
+
+if [ "$#" != 1 ]; then
+    echo "Passed $# parameter(s) instead of 1"
+    echo "usage: ./process_data <05|06|07_and_08|09|10|11|12|all>"
+    exit
+fi
+
 mkdir -p data
-mkdir -p figures
+mkdir -p figures_reproduced
 
 
-./base_scripts/figure5_gen.sh
-./base_scripts/figure6_gen.sh
-./base_scripts/figure7_8_gen.sh
-./base_scripts/figure9_gen.sh
-./base_scripts/figure10_gen.sh
+atleast=0
 
+if [[ "$1" == "05" || "$1" == "all" ]]; then
+atleast=1
+./scripts_gen/figure05_gen.sh
+fi
+
+if [[ "$1" == "06" || "$1" == "all" ]]; then
+atleast=1
+./scripts_gen/figure06_gen.sh
+fi
+
+if [[ "$1" == "07_and_08" || "$1" == "all" ]]; then
+atleast=1
+./scripts_gen/figure07_and_08_gen.sh
+fi
+
+if [[ "$1" == "09" || "$1" == "all" ]]; then
+atleast=1
+./scripts_gen/figure09_gen.sh
+./scripts_gen/table2_and_3_gen.sh
+fi
+
+if [[ "$1" == "10" || "$1" == "all" ]]; then
+atleast=1
+./scripts_gen/figure10_gen.sh
+fi
+
+if [[ "$1" == "11" || "$1" == "all" ]]; then
+atleast=1
+./scripts_gen/figure11_gen.sh
+fi
+
+if [[ "$1" == "12" || "$1" == "all" ]]; then
+atleast=1
+./scripts_gen/figure12_gen.sh
+./scripts_gen/table4_and_5_gen.sh
+fi
+
+if [ "$atleast" == "0" ]; then
+    echo "Passed $1 as parameter"
+    echo "usage: ./process_data <05|06|07_and_08|09|10|11|12|all>"
+    exit
+fi
--- reproducibility/TOMACS2023/run_exp.sh
+++ reproducibility/TOMACS2023/run_exp.sh
@@ -1,9 +1,65 @@
 #!/bin/bash
 
-./figure5_run.sh
-./figure6_run.sh
-./figure7_run.sh
-#./figure8a_run.sh
-#./figure8b_run.sh
-#./figure8c_run.sh
-#./figure9_run.sh
+if [ "$#" != 1 ]; then
+    echo "Passed $# parameter(s) instead of 1"
+    echo "usage: ./run_exp <05|06|07_and_08|09a|09b|09c|10|11|12|all>"
+    exit
+fi
+
+mkdir -p data
+mkdir -p figures_reproduced
+
+
+atleast=0
+
+if [[ "$1" == "05" || "$1" == "all" ]]; then
+atleast=1
+./scripts_run/figure05_run.sh
+fi
+
+if [[ "$1" == "06" || "$1" == "all" ]]; then
+atleast=1
+./scripts_run/figure06_run.sh
+fi
+
+if [[ "$1" == "07_and_08" || "$1" == "all" ]]; then
+atleast=1
+./scripts_run/figure07_and_08_run.sh
+fi
+
+if [[ "$1" == "09a" || "$1" == "all" ]]; then
+atleast=1
+./scripts_run/figure09a_run.sh
+fi
+
+if [[ "$1" == "09b" || "$1" == "all" ]]; then
+atleast=1
+./scripts_run/figure09b_run.sh
+fi
+
+if [[ "$1" == "09c" || "$1" == "all" ]]; then
+atleast=1
+./scripts_run/figure09c_run.sh
+fi
+
+if [[ "$1" == "10" || "$1" == "all" ]]; then
+atleast=1
+./scripts_run/figure10_run.sh
+fi
+
+if [[ "$1" == "11" || "$1" == "all" ]]; then
+atleast=1
+./scripts_run/figure11_run.sh
+fi
+
+if [[ "$1" == "12" || "$1" == "all" ]]; then
+atleast=1
+./scripts_run/figure12_run.sh
+./scripts_run/table4_and_5_run.sh
+fi
+
+if [ "$atleast" == "0" ]; then
+    echo "Passed $1 as parameter"
+    echo "usage: ./run_exp <05|06|07_and_08|09a|09b|09c|10|11|12|all>"
+    exit
+fi
