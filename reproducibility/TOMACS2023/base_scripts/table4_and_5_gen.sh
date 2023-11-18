efa792dbe8c7e50f8ac94646e13617db73a77757
Romolo Marotta <romolo.marotta@gmail.com>
2023-11-18 18:52:14 +0100

    repro(tomacs23): refactored scripts for generating tables
    Signed-off-by: Romolo Marotta <romolo.marotta@gmail.com>

--- reproducibility/TOMACS2023/base_scripts/table4_and_5_gen.sh
+++ reproducibility/TOMACS2023/base_scripts/table4_and_5_gen.sh
@@ -1,12 +1,12 @@
 echo -n "generating data for table 4 and 5..."
-python3 scripts_plot/process_use_locality_folder_for_tables.py results/results_tuberculosis/ > data/table4.csv
+python3 scripts_table/process_use_locality_folder_for_tables.py results/results_tuberculosis/ > data/table4.csv
 echo "Done!"
 
 
 echo -n "generating table 4 ..."
-python generate_table4.py > figures_reproduced/table4.tex
+python scripts_table/generate_table4.py > figures_reproduced/table4.tex
 echo "Done!"
 
 echo -n "generating table 5 ..."
-python generate_table5.py > figures_reproduced/table5.tex
+python scripts_table/generate_table5.py > figures_reproduced/table5.tex
 echo "Done!"
---
+++ reproducibility/TOMACS2023/scripts_table/generate_table5.py
@@ -0,0 +1,53 @@
+#!/bin/python3
+
+fn = 'data/table4.csv'
+
+f = open(fn)
+cnt = 0
+
+res = []
+
+
+
+max_treads='40'
+
+for line in open("thread.conf"):
+    if "MAX_THREADS" in line:
+        line = line.split("=")[-1].split("#")[0].replace('"', '').strip().split(' ')
+        #print(line)
+        max_treads = str(max([int(x) for x in line]))
+
+for line in f.readlines():
+  if 'threads_1-' in line: continue
+  if 'seq' in line: continue
+  if 'test' in line: continue
+  line = line.strip().split(',')[:7]
+  
+  tot = float(line[5])
+  rol = float(line[3])+float(line[4])
+  pro = tot/rol
+  
+
+  res += [(line[0],pro)]
+
+  cnt +=1
+f.close()
+
+res=sorted(res)
+
+sim=["enfl_0-numa_0", "enfl_1-numa_0", "enfl_1-numa_1"]
+
+d={}
+for s in sim:
+  d[s] = 0
+
+for t,p in res:
+  s = "-".join(t.split("-")[:2])
+  d[s] = p
+
+for s in sim:
+  line=s #.replace("_", "\\_")
+  line += " "+f'& {"{:.2f}".format(  d[s])}  '
+  line+=" \\\\ \\hline"
+  line = line.replace("enfl_1-numa_1", '{\\bf cache+NUMA opt.}').replace("enfl_1-numa_0", '{\\bf cache opt.}').replace("enfl_0-numa_0",'{\\bf baseline}')
+  print(line)
