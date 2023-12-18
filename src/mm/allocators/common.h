420499932aab85197daefecb2703f0cd7238dea7
Romolo Marotta <romolo.marotta@gmail.com>
2023-12-18 17:42:02 +0100

    refactor(allocators): added a new parameter for memkind
    Signed-off-by: Romolo Marotta <romolo.marotta@gmail.com>

--- src/mm/allocators/common.h
+++ src/mm/allocators/common.h
@@ -12,23 +12,30 @@ extern void *__real_calloc(size_t, size_t);
 extern int   __real_posix_memalign(void **memptr, size_t alignment, size_t size);
 extern void *__real_aligned_alloc(size_t alignment, size_t size);
 
-#define WRAP 1
+#define WRAP_TO_STD 0
+#define WRAP_TO_REA 1
+#define WRAP_TO_MKD 2
 
-#if WRAP
+#define WRAP WRAP_TO_REA
 
-#define MALLOC __real_malloc
-#define CALLOC __real_calloc
-#define POSIX_MEMALIGN __real_posix_memalign
-#define ALIGNED_ALLOC __real_aligned_alloc
-#define FREE __real_free
+#if WRAP == WRAP_TO_REA
+
+#define MALLOC(x,m)   __real_malloc(x)
+#define CALLOC(x,y,m) __real_calloc(x,y)
+#define POSIX_MEMALIGN(x,y,z,m) __real_posix_memalign(x,y,z)
+#define ALIGNED_ALLOC(x,y,m)    __real_aligned_alloc(x,y)
+#define FREE(x,m)     __real_free(x)
+
+#elif WRAP == WRAP_TO_MKD
+
+#define MALLOC(x,m)   configurable_malloc(x)
+#define CALLOC(x,y,m) configurable_calloc(x,y)
+#define POSIX_MEMALIGN(x,y,z,m) configurable_posix_memalign(x,y,z)
+#define ALIGNED_ALLOC(x,y,m)    configurable_aligned_alloc(x,y)
+#define FREE(x,m)     configurable_free(x)
+
+#endif
 
 #else
-
-#define MALLOC malloc
-#define CALLOC calloc
-#define POSIX_MEMALIGN posix_memalign
-#define ALIGNED_ALLOC aligned_alloc
-#define FREE free
-#endif
 
 #endif
--- src/mm/allocators/thr_alloc.h
+++ src/mm/allocators/thr_alloc.h
@@ -3,11 +3,11 @@
 
 #include <common.h>
 
-static inline void *thr_alloc(size_t size){ return MALLOC(size); }
-static inline void *thr_calloc(size_t nmemb, size_t size){ return CALLOC(nmemb, size); }
-static inline void *thr_aligned_alloc(size_t alignment, size_t size){ return ALIGNED_ALLOC(alignment, size);}
-static inline void  thr_free(void *ptr){ return FREE(ptr); }
-static inline int   thr_memalign_alloc(void **memptr, size_t alignment, size_t size){ return POSIX_MEMALIGN(memptr, alignment, size);}
+static inline void *thr_alloc(size_t size){ return MALLOC(size, DRAM_MEM); }
+static inline void *thr_calloc(size_t nmemb, size_t size){ return CALLOC(nmemb, size, DRAM_MEM); }
+static inline void *thr_aligned_alloc(size_t alignment, size_t size){ return ALIGNED_ALLOC(alignment, size, DRAM_MEM);}
+static inline void  thr_free(void *ptr){ return FREE(ptr, DRAM_MEM); }
+static inline int   thr_memalign_alloc(void **memptr, size_t alignment, size_t size){ return POSIX_MEMALIGN(memptr, alignment, size, DRAM_MEM);}
 
 
 #endif
