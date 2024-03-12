3cf1d02133adecc6fcab00e584d7ee5d663da791
federicamontes <federica.montesan42@gmail.com>
2024-03-12 12:41:30 +0100

    feature(iss-write protection)
    - added incremental_state_saving.h
    - added incremental_state_saving.c
    
    * they contains all the facilities to support the incremental state saving operation
    + TODO: log_incremental and mark_dirty_pages

---
+++ src/core/incremental_state_saving.c
@@ -0,0 +1,272 @@
+#include <stddef.h>
+#include <sys/mman.h>
+#include <assert.h>
+
+#include <ROOT-Sim.h>
+
+#include <mm.h>
+#include <dymelor.h>
+#include <segment.h>
+#include <incremental_state_saving.h>
+
+
+tracking_data **t_data;
+int device_fd;
+
+extern void **mem_areas; /// pointers to the lp segment
+
+lp_iss_metadata *iss_states; /// runtime iss metadata for each lp
+model_t iss_costs_model;	 /// runtime tuning of the cost model 
+
+__thread int __in_log_full = 0;
+
+
+/** syscalls */
+int track_memory(unsigned long address, size_t size){
+	return syscall(PROTECT_MEM,address, size);
+}
+
+int untrack_memory(unsigned long address, size_t size){
+	return syscall(UNPROTECT_MEM,address, size);
+}
+
+int flush_local_tlb(unsigned long address, size_t size){
+	return syscall(FLUSH_LOCAL_TLB,address, size);
+}
+
+/** syscalls' wrappers */
+
+int guard_memory(unsigned int lid) {
+
+	unsigned long size;
+	if(!pdes_config.iss_enabled_mprotection) return NO_PRTCT_ENABLED;
+	assert(size <= PER_LP_PREALLOCATED_MEMORY);
+	return track_memory((unsigned long) mem_areas[lid], size);
+}
+
+int unguard_memory(unsigned int lid) {
+
+	unsigned long size;
+	if(!pdes_config.iss_enabled_mprotection) return NO_PRTCT_ENABLED;
+	assert(size <= PER_LP_PREALLOCATED_MEMORY);
+	return track_memory((unsigned long) mem_areas[lid], size);
+}
+
+int flush(unsigned int lid) {
+
+	unsigned long size;
+	if(!pdes_config.iss_enabled_mprotection) return NO_PRTCT_ENABLED;
+	return flush_local_tlb((unsigned long) mem_areas[lid], size);
+}
+
+
+
+int get_page_idx_from_ptr(unsigned int cur_lp, void *addr){
+	unsigned long long base_addr = (unsigned long long)(mem_areas[cur_lp]);
+	unsigned long long pg_addr = ((unsigned long long)addr) & (~ (PAGE_SIZE-1));
+	assert(pg_addr >= base_addr);
+	assert(pg_addr <  (base_addr+PER_LP_PREALLOCATED_MEMORY));
+	unsigned long long offset = pg_addr - base_addr;
+	assert(offset < PER_LP_PREALLOCATED_MEMORY);
+	return (unsigned int) offset/PAGE_SIZE + PER_LP_PREALLOCATED_MEMORY/PAGE_SIZE;
+}
+
+unsigned int get_lowest_page_from_partition_id(unsigned int page_id){
+	do{
+		page_id<<=1;
+	}while(page_id  < (PER_LP_PREALLOCATED_MEMORY/PAGE_SIZE)*2);
+	return page_id >>=1;
+}
+
+void* get_page_ptr_from_idx(unsigned int cur_lp, unsigned int id){
+	assert(id>=PER_LP_PREALLOCATED_MEMORY/PAGE_SIZE);
+	assert(id<PER_LP_PREALLOCATED_MEMORY*2/PAGE_SIZE);
+	return ((char*)mem_areas[cur_lp]) + (id-PER_LP_PREALLOCATED_MEMORY/PAGE_SIZE)*PAGE_SIZE; 
+}
+
+
+bool is_next_ckpt_incremental(void) {
+
+	if (pdes_config.checkpointing == PERIODIC_STATE_SAVING)  
+		return false;
+
+	/// if too many iss do a full ckpt
+	if (iss_states[current_lp].iss_counter++ == pdes_config.ckpt_forced_full_period) {
+		/// its time to take a full snapshot
+		iss_states[current_lp].iss_counter = 0; 
+		return false;
+	}
+
+	return true;
+
+}
+
+partition_log *log_incremental(unsigned int lid, simtime_t ts) {
+
+	partition_log *ckpt;
+
+	return ckpt;
+}
+
+
+void log_incremental_restore(partition_log *cur) {
+
+
+}
+
+
+void mark_dirty_pages(tracking_data *data) {
+
+	//todo: do some things to mark dirty pages
+	
+
+}
+
+
+/** methods to use klm through ioctl */
+
+void init_segment_monitor_support(tracking_data *data) {
+
+	assert(data != NULL);
+	assert(device_fd != -1);
+
+	ioctl(device_fd, TRACKER_INIT, data);
+}
+
+
+tracking_data *get_fault_info(unsigned int lid) {
+
+	tracking_data *local_data = t_data[lid];
+	unsigned long len;
+	unsigned long *buff;
+	local_data->base_address = (unsigned long) mem_areas[0];
+	local_data->subsegment_address = (unsigned long) mem_areas[lid];
+	local_data->end_address = (unsigned long) (mem_areas[lid]+MAX_MMAP*NUM_MMAP);
+	local_data->len_buf = (unsigned long) NUM_PAGES_PER_MMAP;
+	if(local_data->buff_addresses == NULL) local_data->buff_addresses = malloc(local_data->len_buf * sizeof(unsigned long));
+	local_data->segment_id = SEGID(mem_areas[lid], mem_areas[0], NUM_PAGES_PER_SEGMENT);
+	ioctl(device_fd, TRACKER_GET, &local_data[lid]);
+	if (t_data != NULL) {
+		len = local_data->len_buf;
+		buff = rsalloc(sizeof(unsigned long) * len);
+		mark_dirty_pages(local_data);
+		return local_data;
+	}
+
+	return local_data;
+}
+
+/**  only when protect() is used -- maybe use compilation flags */
+
+
+void dirty(void* addr, size_t size){
+	//printf("%u: lp %u %p\n", tid, current_lp, addr);
+	unsigned int page_id    	= get_page_idx_from_ptr(current_lp, addr);
+	unsigned int cur_id 		= page_id;
+	partition_node_tree_t *tree = &iss_states[current_lp].partition_tree[0];
+    iss_states[current_lp].count_tracked++;
+	unsigned int tgt_partition_size = 0;
+	unsigned int cur_partition_size = 1;
+	unsigned int partition_id = page_id;
+    bool was_dirty = 0;
+    unsigned short cur_dirty_ts =  iss_states[current_lp].cur_virtual_ts;
+
+	while(cur_id > 0){
+        was_dirty = tree[cur_id].dirty == cur_dirty_ts;
+			
+		if(tree[cur_id].valid[0]){
+			tgt_partition_size = cur_partition_size;
+			partition_id = cur_id;
+		}
+        
+        if(!was_dirty){
+            tree[cur_id].dirty = cur_dirty_ts;
+            assert(tree[cur_id].access_count>=0);
+            tree[cur_id].access_count += 1;
+            assert(tree[cur_id].access_count>=0);
+            tree[cur_id].cost = estimate_cost(cur_partition_size, ((float)tree[cur_id].access_count) / ((float)iss_states[current_lp].iss_model_round+1) );
+        }
+        
+		cur_partition_size<<=1;
+		cur_id>>=1;
+	}
+
+	partition_id = get_lowest_page_from_partition_id(partition_id);
+
+	iss_states[current_lp].current_incremental_log_size += tgt_partition_size*PAGE_SIZE;
+	untrack_memory((unsigned long) get_page_ptr_from_idx(current_lp, partition_id), tgt_partition_size*PAGE_SIZE);
+}
+
+
+void sigsev_tracer_for_dirty(int sig, siginfo_t *func, void *arg){
+	assert(sig==SIGSEGV);
+    assert(__in_log_full == 0);
+	(void)arg;
+	dirty(func->si_addr, 1);
+}
+
+
+/** methods to initialize iss support and set tracking_data struct */ 
+
+void set_tracking_data(tracking_data *data, unsigned long start, unsigned long addr, unsigned long end,
+	unsigned int segid, unsigned long len) {
+
+	assert(data->buff_addresses != NULL);
+
+	data->base_address 					= start;
+	data->subsegment_address 			= addr;
+	data->end_address 					= end;
+	data->segment_id 					= segid;
+	data->len_buf 						= len;
+}
+
+
+void init_tracking_data(tracking_data *data) {
+
+	data->base_address 			= 0UL;
+	data->subsegment_address 	= 0UL;
+	data->end_address 			= 0UL;
+	data->segment_id 			= 0UL;
+	data->len_buf 				= NUM_PAGES_PER_SEGMENT;
+	data->buff_addresses 		= rsalloc(data->len_buf * sizeof(unsigned long)); 
+}
+
+void init_incremental_checkpointing_support(unsigned int threads, unsigned int lps) {
+
+	uint i;
+	t_data = malloc(sizeof(struct _tracking_data) * lps);
+	for (i = 0; i < lps; i++)
+		init_tracking_data(t_data[i]);
+
+	/// init model PER-LP (iss_metadata and model)
+	iss_states = (lp_iss_metadata*)rsalloc(sizeof(lp_iss_metadata)*lps + 2*PER_LP_PREALLOCATED_MEMORY/PAGE_SIZE);
+	iss_costs_model.mprotect_cost_per_page = 1; //TODO: costo per protect ?
+	iss_costs_model.log_cost_per_page = 100; 
+
+	if (pdes_config.iss_signal_mprotect) {
+		struct sigaction action;
+		action.sa_sigaction = sigsev_tracer_for_dirty;
+		action.sa_flags = SA_SIGINFO;
+		sigaction(SIGSEGV, &action, NULL);
+	}
+
+
+
+}
+
+void init_incremental_checkpoint_support_per_lp(unsigned int lp){
+
+	bzero(iss_states+lp, sizeof(lp_iss_metadata));
+	
+	//INCR: fill tracking_data struct with addresses and then call ioctl(fd, TRACKER_INIT, t_data)
+	unsigned int segid = SEGID(mem_areas[lp], mem_areas[0], NUM_PAGES_PER_SEGMENT);
+	set_tracking_data(t_data[lp], (unsigned long) mem_areas[0], (unsigned long) mem_areas[lp],
+		(unsigned long) mem_areas[lp] + MAX_MMAP*NUM_MMAP, segid, NUM_PAGES_PER_SEGMENT);
+	
+	//INCR: ioctl(fd, TRACKER_INIT, t_data)
+	init_segment_monitor_support(t_data);
+
+	iss_first_run_model(current_lp); 
+
+
+}
---
+++ src/core/incremental_state_saving.h
@@ -0,0 +1,129 @@
+#ifndef __ISS_H_
+#define __ISS_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
+#include <fcntl.h>
+#include <pthread.h>
+#include <assert.h>
+
+#include <segment.h>
+
+
+#define PROTECT_MEM 134 //this depends on what the kernel tells you when mounting the vtpmo module
+#define UNPROTECT_MEM 156 //this depends on what the kernel tells you when mounting the vtpmo module
+#define FLUSH_LOCAL_TLB 174 //this depends on what the kernel tells you when mounting the vtpmo module
+
+#define NO_PRTCT_ENABLED -1
+
+#define BE_BUFF_SIZE NUM_MMAP
+
+
+#define TRACKER_INIT			(1U << 2) ///ioctl cmd for initialization of the support
+#define TRACKER_GET				(1U << 3) ///ioctl cmd for the retrieval request
+#define TRACKER_DUMP			(1U << 4) ///ioctl cmd for debugging purposes (serial only)
+#define TRACKER_SET_SEGSIZE		(1U << 5) ///ioctl cmd for setting segment size
+
+#define SEGID(addr, base, size) ({unsigned int id = ((addr-base)/PAGE_SIZE + size)/size - 1; id;})
+
+
+/* user data struct to pass data back and forth user/kernel space */
+typedef struct _tracking_data {
+	unsigned long base_address; 		/// base address of segment
+	unsigned long subsegment_address; 	/// base address of subsegment
+	unsigned long end_address;			/// end address of segment
+	unsigned long segment_id;			/// segment id
+	unsigned long *buff_addresses;		/// buffer of addresses
+	unsigned long len_buf;				/// requested buffer's lenght
+} tracking_data;	
+
+/** This struct keeps metadata of a partition log */
+typedef struct __partition_log{
+	size_t size;
+	struct __partition_log *next;
+	simtime_t ts;
+	char *addr;
+	char *log;
+}partition_log;
+
+
+/// This struct keeps parameters for the iss cost model
+typedef struct __model{
+	float mprotect_cost_per_page;
+	float log_cost_per_page;
+}model_t;
+
+/// This struct keeps runtime info for an admittable partition of the state segment
+typedef struct __partition_tree_node{
+	float cost;
+	int access_count;
+	char valid[2];
+	unsigned short dirty;
+}partition_node_tree_t;
+
+
+/// This struct keeps all metadata for incremental state saving of a model state
+typedef struct __per_lp_iss_metadata{
+	//partition_node_tree_t partition_tree[2*PER_LP_PREALLOCATED_MEMORY/PAGE_SIZE];
+	ssize_t current_incremental_log_size;
+	int iss_counter;
+    int iss_model_round;
+    int count_tracked;
+    int disabled;
+    unsigned short cur_virtual_ts;
+    char current_model;
+	partition_node_tree_t partition_tree[]; //todo: when alloc per_lp_iss_metadata add 2*PER_LP_PREALLOCATED_MEMORY/PAGE_SIZE
+}lp_iss_metadata;
+
+extern tracking_data **t_data;
+
+extern lp_iss_metadata *iss_states; /// runtime iss metadata for each lp
+extern model_t iss_costs_model;	 /// runtime tuning of the cost model 
+
+/** methods for incremental state saving support */
+void init_incremental_checkpointing_support(unsigned int threads, unsigned int lps);
+void init_tracking_data(tracking_data *);
+void set_tracking_data(tracking_data *data, unsigned long start, unsigned long addr, unsigned long end,
+										unsigned int segid, unsigned long len);
+void init_incremental_checkpoint_support_per_lp(unsigned int lp);
+
+/** methods for incremental state saving */
+bool is_next_ckpt_incremental();
+partition_log *log_incremental(unsigned int lid, simtime_t ts);
+void log_incremental_restore(partition_log *cur);
+tracking_data *get_fault_info(unsigned int lid);
+void mark_dirty_pages(tracking_data *data);
+void init_segment_monitor_support(tracking_data *data);
+
+
+void sigsev_tracer_for_dirty(int sig, siginfo_t *func, void *arg);
+void dirty(void *, size_t);
+
+/** methods for model management */
+void iss_first_run_model(unsigned int current_lp); 
+void iss_log_incremental_reset(unsigned int lp);
+void iss_update_model(unsigned int cur_lp);
+float estimate_cost(size_t size, float probability);
+
+
+/** syscalls wrapper */
+int guard_memory(unsigned int lid);
+int unguard_memory(unsigned int lid);
+int flush(unsigned int lid);
+
+/** syscalls */
+int track_memory(unsigned long address, size_t size);
+int untrack_memory(unsigned long address, size_t size);
+int flush_local_tlb(unsigned long address, size_t size);
+
+
+#endif
