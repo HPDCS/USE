ad6a0451b459f4e3691e203db2137c88290172ff
federicamontes <federica.montesan42@gmail.com>
2024-03-12 12:37:51 +0100

    feature(iss-write protection)
    - restored macro SCALE with config parameter pdes_config.segment_shift

--- src/mm/segment.h
+++ src/mm/segment.h
@@ -7,13 +7,14 @@
 
 
 #define SCALE (pdes_config.segment_shift)
+//#define SCALE 4ULL //default is 4 in segment_shift but cannot allocate array with config parameter
 #define NUM_PAGES_PER_SEGMENT ((512ULL * 512ULL)>>SCALE)
 
 #define PER_LP_PREALLOCATED_MEMORY (NUM_PAGES_PER_SEGMENT * PAGE_SIZE ) /// This should be power of 2 multiplied by a page size. This is 1GB per LP.
 
 #define NUM_PAGES_PER_MMAP	(NUM_PAGES_PER_SEGMENT >> 2)
 #define MAX_MMAP			(NUM_PAGES_PER_MMAP * PAGE_SIZE) /// This is the maximum amount of memory that a single mmap() call is able to serve. TODO: this should be checked within configure.ac
-#define NUM_MMAP			(((PER_LP_PREALLOCATED_MEMORY) / MAX_MMAP) == 0 ? 1 : ((PER_LP_PREALLOCATED_MEMORY) / MAX_MMAP))  
+#define NUM_MMAP			(((PER_LP_PREALLOCATED_MEMORY) / MAX_MMAP) <= 0 ? 1 : ((PER_LP_PREALLOCATED_MEMORY) / MAX_MMAP))  
 
 
 #endif
