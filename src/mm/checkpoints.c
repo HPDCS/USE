31dc9e4548847d222e8b6cea31d1a132741f42dc
federicamontes <federica.montesan42@gmail.com>
2024-04-03 10:29:20 +0200

    debugging log size for pcs model

--- src/mm/checkpoints.c
+++ src/mm/checkpoints.c
@@ -182,12 +182,6 @@ void *log_full(int lid) {
 		printf("AFTER BITMAP BLOCKS partial_size %lu\n", partial_size);
 
 
-		// Sanity check
-		if ((char *)ckpt + partial_size != ptr){
-			rootsim_error(true, "Actual (full) ckpt size is wrong by %d bytes!\nlid = %d ckpt = %p size = %#x (%d), ptr = %p, ckpt + size = %p\n", (char *)ckpt + size - (char *)ptr, lid, ckpt, size, size, ptr, (char *)ckpt + size);
-		}
-
-
 		/// if log is full
 		if (!recoverable_state[lid]->is_incremental) {
 
