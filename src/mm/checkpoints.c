f63721761af6cfd579754ac1dd736067c0d32077
federicamontes <federica.montesan42@gmail.com>
2024-04-03 10:23:42 +0200

    debugging log size for pcs model

--- src/mm/checkpoints.c
+++ src/mm/checkpoints.c
@@ -98,7 +98,7 @@ void *log_full(int lid) {
 	recoverable_state[lid]->is_incremental = is_next_ckpt_incremental(); /// call routine for determining the type of checkpointing
 	size = get_log_size(recoverable_state[lid]);
 
-	//printf("LOG_FULL size ckpt %lu\n", size);
+	printf("LOG_FULL size ckpt %lu\n", size);
 
 	ckpt = rsalloc(size);
 
@@ -116,7 +116,7 @@ void *log_full(int lid) {
 	((malloc_state*)ckpt)->timestamp = current_lvt;
 
 	partial_size += sizeof(malloc_state);
-	//printf("AFTER MALLOC STATE partial_size %lu\n", partial_size);
+	printf("AFTER MALLOC STATE partial_size %lu\n", partial_size);
 
 	// Copy the per-LP Seed State (to make the numerical library rollbackable and PWD)
 	memcpy(ptr, &LPS[lid]->seed, sizeof(seed_type));
@@ -124,7 +124,7 @@ void *log_full(int lid) {
 
 	partial_size += sizeof(seed_type);
 
-	//printf("AFTER SEED partial_size %lu\n", partial_size);
+	printf("AFTER SEED partial_size %lu\n", partial_size);
 
 
 	if(recoverable_state[lid]->is_incremental){
@@ -136,7 +136,7 @@ void *log_full(int lid) {
 		*((void ** )ptr) = partial_log;
 		ptr = (void *) ((char *) ptr + sizeof(void *));
 		partial_size += sizeof(void *);
-		//printf("AFTER LOG INCREMENTAL partial_size %lu\n", partial_size);
+		printf("AFTER LOG INCREMENTAL partial_size %lu\n", partial_size);
 
 	}
     else{
@@ -172,7 +172,7 @@ void *log_full(int lid) {
 		ptr = (void*)((char*)ptr + sizeof(malloc_area));
 
 		partial_size += sizeof(malloc_area);
-		//printf("AFTER MALLOC AREA partial_size %lu\n", partial_size);
+		printf("AFTER MALLOC AREA partial_size %lu\n", partial_size);
 
 
 		// Sanity check
@@ -184,7 +184,7 @@ void *log_full(int lid) {
 		ptr = (void*)((char*)ptr + bitmap_blocks * BLOCK_SIZE);
 
 		partial_size += bitmap_blocks * BLOCK_SIZE;
-		//printf("AFTER BITMAP BLOCKS partial_size %lu\n", partial_size);
+		printf("AFTER BITMAP BLOCKS partial_size %lu\n", partial_size);
 
 
 		// Sanity check
