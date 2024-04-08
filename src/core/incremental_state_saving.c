ba4d58fda338de9bd36f471848f391c522bc0dbb
federicamontes <federica.montesan42@gmail.com>
2024-04-08 10:59:08 +0200

    added printf in log_incremental_restore

--- src/core/incremental_state_saving.c
+++ src/core/incremental_state_saving.c
@@ -514,6 +514,7 @@ partition_log *log_incremental(unsigned int cur_lp, simtime_t ts) {
 void log_incremental_restore(partition_log *cur) {
 
 	while(cur){
+		printf("[log_incremental_restore] cur %p\n", cur);
 		memcpy(cur->addr, cur->log, cur->size);
 		cur = cur->next;
 	}
