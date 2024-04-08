82069a94ccd2dfd2ec448473a3cd603a47d931b9
federicamontes <federica.montesan42@gmail.com>
2024-04-08 10:21:24 +0200

    refactor(incremental_state_saving.c)
    - added check on prev_log before assignment -- debugging

--- src/core/incremental_state_saving.c
+++ src/core/incremental_state_saving.c
@@ -327,7 +327,7 @@ partition_log * log_incremental_no_tree(unsigned int cur_lp, simtime_t ts) {
 			cur_log->ts = ts;
 			cur_log->addr = get_page_ptr_from_idx(cur_lp, i);
 			cur_log->log = rsalloc(cur_log->size);
-			prev_log = cur_log; 
+			if (prev_log == NULL) prev_log = cur_log; 
 
 			//printf("[log_incremental] CKPT tgt_id %u \t addr %p \t cur_log %p \t log %p \t size %lu\n", 
 			//	i, cur_log->addr, cur_log, cur_log->log, iss_states[cur_lp].current_incremental_log_size);
