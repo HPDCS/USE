60a38d89073e3fa2b3ca2aead1d112d39752fb99
federicamontes <federica.montesan42@gmail.com>
2024-04-11 10:45:31 +0200

    removed useless get_fault_info call in log_full

--- src/mm/checkpoints.c
+++ src/mm/checkpoints.c
@@ -249,7 +249,6 @@ void *log_state(int lid) {
 	if (pdes_config.checkpointing == INCREMENTAL_STATE_SAVING) {
 		size_t logsize = iss_states[lid].current_incremental_log_size;
 		//INCR: compute size to protect
-		get_fault_info(lid); /// TODO: mark dirty pages
 		ckpt = log_full(lid);
 		//INCR: todo update model
 		iss_update_model(lid);
