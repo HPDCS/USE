8c27e1cfe332c6a2196645d3f5cefcf286bfab62
federicamontes <federica.montesan42@gmail.com>
2024-04-11 11:23:47 +0200

    added lp id in printf in incremental_log_restore

--- src/core/incremental_state_saving.c
+++ src/core/incremental_state_saving.c
@@ -514,7 +514,7 @@ partition_log *log_incremental(unsigned int cur_lp, simtime_t ts) {
 void log_incremental_restore(partition_log *cur) {
 
 	while(cur){
-		printf("[log_incremental_restore] cur %p -- addr %lu\n", cur, cur->addr);
+		printf("lp %u : [log_incremental_restore] cur %p -- addr %lu\n", current_lp, cur, cur->addr);
 		memcpy(cur->addr, cur->log, cur->size);
 		cur = cur->next;
 	}
