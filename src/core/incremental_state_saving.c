73dc3465d60c858996fb075223f719d1f73fa21b
federicamontes <federica.montesan42@gmail.com>
2024-04-17 10:22:44 +0200

    edited printf -- debugging

--- src/core/incremental_state_saving.c
+++ src/core/incremental_state_saving.c
@@ -312,7 +312,7 @@ partition_log * log_incremental_no_tree(unsigned int cur_lp, simtime_t ts) {
 			cur_log->next = prev_log;
 			cur_log->ts = ts;
 			cur_log->addr = get_page_ptr_from_idx(cur_lp, i);
-			printf("BITMAP ADDRESS i %d \t address %lu - %p\n", i, (unsigned long )cur_log->addr, (void *) cur_log->addr);
+			printf("BITMAP ADDRESS i %d -- %ld \t address %lu - %p\n", i, i%NUM_PAGES_PER_SEGMENT ,(unsigned long )cur_log->addr, (void *) cur_log->addr);
 			cur_log->log = rsalloc(cur_log->size);
 			prev_log = cur_log; 
 
