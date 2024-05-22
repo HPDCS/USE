b50161a64496a347c7106d8bf2011c1b33c2db86
federicamontes <federica.montesan42@gmail.com>
2024-05-22 16:50:33 +0200

    feature(pte materialization)
    - added write on first byte of each page in segment to force materialization

--- src/mm/segment.c
+++ src/mm/segment.c
@@ -95,15 +95,16 @@ void *get_segment(GID_t gid, unsigned int numa_node, void ***pages) {
 		for(j=0;j<MAX_MMAP/PAGE_SIZE;j++,h++)
 			(*pages)[h] = (char *)the_address+h*PAGE_SIZE;
 
+		if (pdes_config.iss_enabled_mprotection) {
+			for (j=0; j < MAX_MMAP/PAGE_SIZE; j+=PAGE_SIZE)
+				*((char *)mmapped[i] + j) = 'x';
+		}
+
 		the_address = (char *)the_address + MAX_MMAP;
 
 
 	}
 
-	if (pdes_config.iss_enabled_mprotection) {
-		for (i=0; i < PER_LP_PREALLOCATED_MEMORY; i+=NUM_PAGES_PER_SEGMENT)
-			*((char *)mmapped[0] + i) = 'x';
-	}
 
 	return mmapped[0];
 }
