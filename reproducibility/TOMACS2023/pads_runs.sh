38548633e1019cbcc67141153204cfe91f9d20e9
Romolo Marotta <romolo.marotta@gmail.com>
2025-01-30 20:20:51 +0000

    updated scripts for testing

---
+++ reproducibility/TOMACS2023/pads_runs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source autoconf.sh
+
+TA_LIST="0.1 0.2 0.8"
+TH_LIST="8 16 24 32 40"
+for j in ${TA_LIST}; do
+for i in ${TH_LIST}; do
+./scripts_run/use_pads2025_pcs.sh  $j $i 4096
+done
+done
+
+
+SCALING="1.5 1.0 0.25"
+TH_LIST="8 16 24 32 40"
+for j in ${SCALING}; do
+for i in ${TH_LIST}; do
+./scripts_run/use_pads2025_highway.sh  $j $i 4096
+done
+done
+
+
